34bf8f1aa46ccf44495629fc65d32aefd054b55b
RickyC0626 <rickychon99@gmail.com>
2020-06-02 20:50:58 -0400

    Updates

---
+++ precache-manifest.afd7e6008fab85525eb4542801b76b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0629bfd1c3469714b62c08970f84b4f7",
+    "url": "/react-contact-cards/index.html"
+  },
+  {
+    "revision": "efad5582cbedd9034422",
+    "url": "/react-contact-cards/static/css/main.16aff3a2.chunk.css"
+  },
+  {
+    "revision": "31150e4e4cc4685b5075",
+    "url": "/react-contact-cards/static/js/2.2fa153ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-contact-cards/static/js/2.2fa153ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efad5582cbedd9034422",
+    "url": "/react-contact-cards/static/js/main.c1018874.chunk.js"
+  },
+  {
+    "revision": "1f973438f92da38f92a3",
+    "url": "/react-contact-cards/static/js/runtime-main.70ef9f6c.js"
+  }
+]);
