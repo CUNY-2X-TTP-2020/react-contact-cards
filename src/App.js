d3e517e7048c49adc2a1a11068503c1a7711a3a2
RickyC0626 <rickychon99@gmail.com>
2020-06-03 01:11:11 -0400

    Add ContactCard to homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import ContactCard from './components/ContactCard';
+
 function App() 
 {
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Contact Cards</h1>
+
+                <section>
+                    <ContactCard />
+                </section>
             </header>
         </div>
     );
