eb6035828bdc3d03a7da58dee385983dc61b9cfd
RickyC0626 <rickychon99@gmail.com>
2020-06-03 00:25:00 -0400

    Add ContactCard template class

---
+++ src/components/ContactCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ContactCard extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+}
