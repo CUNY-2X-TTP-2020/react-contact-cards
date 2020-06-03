5bcfeb0b1e16604d3ebb1d399e4c6063a07cc0b1
RickyC0626 <rickychon99@gmail.com>
2020-06-03 00:57:06 -0400

    Add ContactCard props

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -6,5 +6,20 @@ export default class ContactCard extends Component
     constructor(props)
     {
         super(props);
+        this.state =
+        {
+            name: props.name !== undefined ? props.name : "John Doe",
+            mobile: props.mobile !== undefined ? props.mobile : "000-000-0000",
+            work: props.work !== undefined ? props.work : "",
+            email: props.email !== undefined ? props.email : "email@domain.com"
+        };
     }
 }
+
+ContactCard.propTypes =
+{
+    name: PropTypes.string.isRequired,
+    mobile: PropTypes.string.isRequired,
+    work: PropTypes.string,
+    email: PropTypes.string.isRequired
+};
