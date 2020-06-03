30e7d5047524424fe167b94757bd2c59f4a6a7d9
RickyC0626 <rickychon99@gmail.com>
2020-06-03 01:50:52 -0400

    Add cards with props

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ function App()
 
                 <section>
                     <ContactCard />
+                    <ContactCard 
+                        name="Jane Doe" 
+                        mobile="111-111-1111"
+                        work="222-222-2222"
+                        email="jane@work.net"
+                    />
+                    <ContactCard 
+                        name="Random Dude"
+                        mobile="333-333-3333"
+                        work="444-444-4444"
+                        email="dude@me.io"
+                    />
                 </section>
             </header>
         </div>
